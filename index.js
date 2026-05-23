b38ac13215f8bd480e745db0a7add13cf511a6fe
Sifen Abduselam <sifenabduselam7@gmail.com>
2026-05-23 05:11:19 -0700

    conditional

--- index.js
+++ index.js
@@ -1,75 +1,7 @@
-// Selectors
-const todoInput = document.querySelector('.todo-input')
-const todoButton = document.querySelector('.todo-button')
-const todoList = document.querySelector('.todo-list')
+let age = 18;
 
+let hasId = true;
 
-
-//  add an EventListeners
-todoButton.addEventListener('click', addTodo);
-todoList.addEventListener('click', deleteCheck)
-
-// Functions this function is called when sth happened 
-// and the thing that happens is called event 
-function addTodo(event){
-
-    event.preventDefault();
-// TODO DIV
-const todoDiv = document.createElement('div')
-todoDiv.classList.add('todo')
-// Create LI
-const newTodo = document.createElement('li')
-
-// to safely show user tasks innerText
-newTodo.innerText = todoInput.value;
-
-newTodo.classList.add('todo-item')
-todoDiv.appendChild(newTodo) 
-
-
-// CHECKMARK BUTTON
-const completedButton = document.createElement('button')
-completedButton.innerHTML = 'C';
-completedButton.classList.add("complete-btn");
-todoDiv.appendChild(completedButton)
-
-
-// trash buttn
-
-const trashButton = document.createElement('button')
-trashButton.innerHTML = 'D';
-trashButton.classList.add("trash-btn");
-todoDiv.appendChild(trashButton)
-
-// APPEND TO LIST
-todoList.appendChild(todoDiv)
-// clear TODO INPUT VALUE
-
-todoInput.value = ""
-
-}
-
-
-function deleteCheck(e)
-{
-    const item = e.target;
-
-    if(item.classList[0] === "trash-btn"){
-        const todo = item.parentElement;
-
-        // Animation
-        todo.classList.add("fall")
-        todo.addEventListener('transitioned', function() {
-            todo.remove();
-
-        })
-    }
-
-
-    // CHECKMARK
-//  checkmark
-if(item.classList[0] === "complete-btn") {
-    const todo = item.parentElement;
-    todo.classList.toggle("completed-btn")
-}
+if(age >= 18) {
+    console.log("You may enter the club")
 }
