292081406ac262037ff7b4d6a5af5f1b6d6911f2
Sifen Abduselam <sifenabduselam7@gmail.com>
2026-05-23 20:56:47 -0700

    while

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ let hasMembership = true;
 let str = loggedIn && hasMembership ? "show the video" : "don't show the video";
 
 console.log(str);
+
+while(counter <= 5) {
+    console.log("while loop ran");
+
+    counter = counter + 1;
+
+}
+
+console.log("while loop finishd running")
