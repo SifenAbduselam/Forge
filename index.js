deabf01d755882602edba1b445fde9919568b4e6
Sifen Abduselam <sifenabduselam7@gmail.com>
2026-06-30 01:47:33 -0700

    filter method

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 //  so array means a distance from the beginning 
 
 
-const students = ["Ali", "sara", "John"]
+const numbers = [2, 5, 8, 11];
 
-const result = students.pop("sara")
+const result = numbers.filter(function(n){
+    return n > 5;
+})
+
 
 console.log(result)
-
